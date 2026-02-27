648121de7e3c7abfd64cd9b178c6d0f567554725
raushan <rajraushan7063@gmail.com>
2026-02-28 01:19:31 +0530

    prod

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,52 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from './routes/auth.js';
 import bookingRoutes from './routes/bookings.js';
 import contactRoutes from './routes/contact.js';
-import path from "path"
-import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.NODE_ENV === "production"
+        ? true   // allow same origin in production
+        : "http://localhost:5173",
     credentials: true,
 }));
-app.use(express.json()); 
+
+app.use(express.json());
+
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/contact', contactRoutes);
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Fixora API is running ✅' });
+});
+
+
+
+if (process.env.NODE_ENV === "production") {
+    const clientPath = path.join(__dirname, "../frontend/dist");
+
+    app.use(express.static(clientPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(clientPath, "index.html"));
+    });
+}
+
 
 
 
@@ -35,30 +60,17 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: 'Internal server error' });
 });
 
-const PORT = process.env.PORT || 5000;
 
-mongoose
-    .connect(process.env.MONGO_URI)
+
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        console.log(' Connected to MongoDB');
-        if(process.env.NODE_ENV==="production"){
-            const clientPath=path.join(__dirname,"../frontend/dist");
-            app.use(express.static(clientPath))
-            app.use((rq,res)=>{
-                res.sendFile(path.join(clientPath,"index.html"))
-            })
-        }
-        else {
-            
-            app.listen(PORT, () => {
-                console.log(` Server running on http://localhost:${PORT}`);
-            });
-            app.get('/', (req, res) => {
-    res.json({ message: 'Fixora2 API is running ✅' });
-});
-        }
+        console.log("Connected to MongoDB");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
     })
     .catch((err) => {
-        console.error(' MongoDB connection failed:', err.message);
+        console.error("MongoDB connection failed:", err.message);
         process.exit(1);
     });
